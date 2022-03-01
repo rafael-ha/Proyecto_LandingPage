ee147a88b6914ed5b8ab45037b44495b83ec4151
rafael-ha <rherrera.alumno@iesrafaelalberti.com>
2022-03-01 17:13:00 +0100

    Añadido package.json y archivos media.

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const bloque    = document.querySelectorAll('.bloque')
+const h2        = document.querySelectorAll('.h2')
+    
+h2.forEach( ( cadaH2 , i )=>{
+    h2[i].addEventListener('click', ()=>{
+        bloque.forEach( ( cadaBloque , i )=>{
+            bloque[i].classList.remove('activo')
+        })
+        bloque[i].classList.add('activo')
+
+    })
+})
