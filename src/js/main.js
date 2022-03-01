d05548d70058ac4e8899c5af7462b0792c253c90
rafael-ha <rherrera.alumno@iesrafaelalberti.com>
2022-03-01 21:47:43 +0100

    Elementos implementados: video, carrusel de imagenes, acordeon con personajes, audio al hacer click en los personajes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,36 @@
-'use strict'
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.querySelector('.hamburger');
+    let nav1 = document.querySelector(".navbar-phone");
 
-const bloque    = document.querySelectorAll('.bloque')
-const h2        = document.querySelectorAll('.h2')
-    
-h2.forEach( ( cadaH2 , i )=>{
-    h2[i].addEventListener('click', ()=>{
-        bloque.forEach( ( cadaBloque , i )=>{
-            bloque[i].classList.remove('activo')
-        })
-        bloque[i].classList.add('activo')
+    btn.addEventListener('click', () => {
+        nav1.classList.toggle('show');
+    });
+});
 
+const enlaces = document.querySelectorAll('.play');
+const audiohumano = document.querySelector("#audiohumano");
+const audioboss = document.querySelector("#audioboss")
+
+enlaces.forEach((cadaid, i) => {
+    enlaces[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.id == "humano") {
+            audiohumano.play();
+        } else if (e.target.id == "boss") {
+            audioboss.play();
+        }
     })
 })
+
+const bloque = document.querySelectorAll('.bloque');
+const h3 = document.querySelectorAll('.h3');
+
+h3.forEach((cadah3, i) => {
+    h3[i].addEventListener('click', () => {
+        bloque.forEach((cadaBloque, i) => {
+            bloque[i].classList.remove('activo');
+        });
+        bloque[i].classList.add('activo');
+
+    });
+});
